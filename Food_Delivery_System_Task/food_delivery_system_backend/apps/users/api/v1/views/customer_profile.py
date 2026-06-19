ba04947045b7f31683e69e8c92a647a62d857628
pragnakalpdev85 <pragnakalp.dev85@gmail.com>
2026-06-19 11:23:43 +0530

    feat: created address apis for customer profile

--- Food_Delivery_System_Task/food_delivery_system_backend/apps/users/api/v1/views/customer_profile.py
+++ Food_Delivery_System_Task/food_delivery_system_backend/apps/users/api/v1/views/customer_profile.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import action
 from apps.users.services import CustomerService
 from apps.users.selectors import ProfileSelectors
 from apps.common.utils.custom_responses import success_response
-from apps.users.api.v1.serializers import CustomerProfileSerializer
+from apps.users.api.v1.serializers import CustomerProfileSerializer, CustomerAddressSerializer, AddressSerializer
 from apps.common.api.pagination import CustomerProfilePageNumberPagination
 from apps.common.utils.permissions import IsProfileOwner, IsOwnerOrReadOnly
 from apps.users.models import CustomerProfile
@@ -171,6 +171,140 @@ class CustomerProfileViewSet(viewsets.ModelViewSet):
             "Avatar Uploaded successfully",
             data=serializer.data,
             status_code=status.HTTP_200_OK
+        )
+
+    @extend_schema(
+        summary="Retrieves list of customer's saved addresses",
+        description="Retrieves list of customer's saved addresses from saved address json field",
+        responses={
+            200: CustomerAddressSerializer,
+            404: OpenApiResponse(description="Profile not found")
+        },
+        tags=['CustomerProfile'])
+    @action(
+        detail=True, 
+        methods=['get'],
+        permission_classes=[IsAuthenticated, IsProfileOwner],
+        serializer_class=CustomerAddressSerializer,
+        url_path="list-addresses"
+    )
+    def list_addresses(self, request):
+        """Get all addresses."""
+        customer_profile = self.get_object()
+        serializer = self.get_serializer(customer_profile)
+        return success_response(
+            message="List of saved addresses retrieved successfully",
+            data=serializer.data,
+            status_code=status.HTTP_200_OK
+        )
+
+    @extend_schema(
+        summary="Creates a new customer's saved addresses",
+        description="Creates a new customer's saved addresses for saved address json field",
+        responses={
+            200: CustomerAddressSerializer,
+            404: OpenApiResponse(description="Profile not found")
+        },
+        tags=['CustomerProfile'])
+    @action(
+        detail=True, 
+        methods=['post'],
+        permission_classes=[IsAuthenticated, IsProfileOwner],
+        serializer_class=CustomerAddressSerializer,
+        url_path="create-address"
+    )
+    def create_address(self, request):
+        """Add a new address to the JSON list."""
+        customer_profile = self.get_object()
+        serializer = AddressSerializer(data=request.data)
+        
+        serializer.is_valid(raise_exception=True)
+        address_data = serializer.validated_data
+        address_data['id'] = str(uuid.uuid4()) # Generate unique ID for management
+        
+        # Initialize list if null
+        if not customer_profile.saved_address: 
+            customer_profile.saved_address = []
+            
+        customer_profile.saved_address.append(address_data)
+        customer_profile.save()
+        serializer = self.get_serializer(customer_profile.saved_address)
+        return success_response(
+            message="Address created successfully.",
+            data=serializer.data, 
+            status=status.HTTP_201_CREATED
+        )
+
+    @extend_schema(
+        summary="Updates a customer's saved addresses",
+        description="Updates a customer's saved addresses for saved address json field",
+        responses={
+            200: CustomerAddressSerializer,
+            404: OpenApiResponse(description="Profile not found")
+        },
+        tags=['CustomerProfile'])
+    @action(
+        detail=True, 
+        methods=['put', 'patch'], 
+        permission_classes=[IsAuthenticated, IsProfileOwner],
+        serializer_class=CustomerAddressSerializer,
+        url_path='update-address/(?P<address_id>[^/.]+)'
+    )
+    def update_address(self, request, address_id=None):
+        """Update an existing address by its ID."""
+        customer_profile = self.get_object()
+        addresses = customer_profile.saved_address or []
+        
+        # Find target address
+        address_index = next((index for (index, d) in enumerate(addresses) if d.get('id') == address_id), None)
+        
+        if address_index is None:
+            return Response({"detail": "Address not found."}, status=status.HTTP_404_NOT_FOUND)
+            
+        serializer = AddressSerializer(data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        # Merge old data with updated fields
+        updated_data = {**addresses[address_index], **serializer.validated_data}
+        addresses[address_index] = updated_data
+        
+        customer_profile.saved_address = addresses
+        customer_profile.save()
+        serializer = self.get_serializer(customer_profile.saved_address)
+        return success_response(
+            message="Address updated successfully",
+            data=serializer.data, 
+            status_code=status.HTTP_200_OK
+        )
+
+    @extend_schema(
+        summary="Deletes a customer's saved addresses",
+        description="Delete a customer's saved addresses from saved address json field",
+        responses={
+            204: OpenApiResponse(description="No Content"),
+            404: OpenApiResponse(description="Profile not found")
+        },
+        tags=['CustomerProfile'])
+    @action(
+        detail=True, 
+        methods=['delete'], 
+        permission_classes=[IsAuthenticated, IsProfileOwner],
+        url_path='delete-address/(?P<address_id>[^/.]+)'
+    )
+    def delete_address(self, request, address_id=None):
+        """Remove an address by its ID."""
+        customer_profile = self.get_object()
+        addresses = customer_profile.saved_address or []
+        
+        new_addresses = [addr for addr in addresses if addr.get('id') != address_id]
+        
+        if len(addresses) == len(new_addresses):
+            return Response({"detail": "Address not found."}, status=status.HTTP_404_NOT_FOUND)
+            
+        customer_profile.saved_address = new_addresses
+        customer_profile.save()
+        return success_response(
+            message="Address deleted successfully",
+            status_code=status.HTTP_204_NO_CONTENT
         )
 
         
